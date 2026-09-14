16e9e85975d4cd723f98b0935e5a22802637df8f
Fazle Rabbi <banglamaps@gmail.com>
2026-09-14 23:21:30 +0600

    Keep monthly reviews inside employee performance cards

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,7 +6,6 @@ const nav = [
   ['Career Levels', '/career-levels'],
   ['Roles', '/roles'],
   ['Review Process', '/review-process'],
-  ['Monthly Reviews', '/monthly-reviews/ifrat'],
   ['Team', '/team'],
 ] as const;
 
